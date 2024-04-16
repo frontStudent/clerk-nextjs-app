5bb9690a2a9a25265d862c0a60109c409d5083b4
shiyubei <shiyubei@qince.com>
2024-04-16 13:54:07 +0800

    feat: 添加工作区

---
+++ app/workspace/page.tsx
@@ -0,0 +1,7 @@
+export default async function DashboardPage() {
+  return (
+    <div className="px-8 py-12 sm:py-16 md:px-20">
+      <h1 className="text-3xl font-bold">workspace</h1>
+    </div>
+  );
+}
