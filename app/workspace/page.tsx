0d6c9176a747452e301d25cb6cda8a9a9bd832bb
shiyubei <shiyubei@qince.com>
2024-04-17 17:48:58 +0800

    feat: test

--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -3,7 +3,7 @@ import ImageUploader from "./components/ImageUploader";
 export default async function Form() {
   return (
     <div className="px-8 py-12 sm:py-16 md:px-20">
-      <h1 className="text-3xl font-bold">workspace test</h1>
+      <h1 className="text-3xl font-bold">workspace</h1>
       <ImageUploader />
     </div>
   );
