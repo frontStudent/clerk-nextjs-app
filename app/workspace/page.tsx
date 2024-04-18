bbc31c8a5f41ab6056ae939bb6563136c2431c35
shiyubei <shiyubei@qince.com>
2024-04-18 09:57:36 +0800

    test: deploy

--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -1,6 +1,6 @@
 import ImageUploader from "./components/ImageUploader";
 
-export default async function Form() {
+export default function Workspace() {
   return (
     <div className="px-8 py-12 sm:py-16 md:px-20">
       <h1 className="text-3xl font-bold">workspace</h1>
