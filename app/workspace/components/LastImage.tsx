a4ff16f5d16881f8f2069b32f8bab513998c7f19
shiyubei <shiyubei@qince.com>
2024-04-18 10:08:05 +0800

    test: 图片上传

--- app/workspace/components/LastImage.tsx
+++ app/workspace/components/LastImage.tsx
@@ -1,22 +1,25 @@
 import { list } from "@vercel/blob";
 import Image from "next/image";
 
-async function allImages() {
-  const images = await list();
-  const len = images.blobs.length;
-  return images.blobs[len - 1];
-}
-
-export default async function Form() {
+export async function Images() {
+  async function allImages() {
+    const blobs = await list();
+    return blobs;
+  }
   const images = await allImages();
+
   return (
-      <Image
-        priority
-        key={images.pathname}
-        src={images.url}
-        alt="Image"
-        width={200}
-        height={200}
-      />
+    <section>
+      {images.blobs.map((image) => (
+        <Image
+          priority
+          key={image.pathname}
+          src={image.url}
+          alt="Image"
+          width={200}
+          height={200}
+        />
+      ))}
+    </section>
   );
 }
