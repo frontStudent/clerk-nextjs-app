debff27130209e700531e6e81af332f7f39ff12e
shiyubei <shiyubei@qince.com>
2024-04-18 10:02:42 +0800

    test: 注释图片上传

--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -1,10 +1,10 @@
-import ImageUploader from "./components/ImageUploader";
+// import ImageUploader from "./components/ImageUploader";
 
 export default function Workspace() {
   return (
     <div className="px-8 py-12 sm:py-16 md:px-20">
       <h1 className="text-3xl font-bold">workspace</h1>
-      <ImageUploader />
+      {/* <ImageUploader /> */}
     </div>
   );
 }
